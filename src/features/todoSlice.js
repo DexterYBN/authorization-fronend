983d75db1470cf808c5321fe3c3c38c6bacaadc5
Islam Yakubov <yakubov959@mail.ru>
2023-02-02 23:33:41 +0300

    Update todoSlice.js

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { serverUrl } from '../serverUrl';
+import { serverUrl } from '../serverUrl.js';
 
 // Начальный state
 const initialState = {
